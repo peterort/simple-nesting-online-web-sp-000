3cb143903af1c9f8657c75effb81d35a3cc5b1cb
Peter Ort <peter@peterort.com>
2020-05-14 19:19:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages].to_s
+string = programmer_hash[:dennis_ritchie][:languages]
+string.to_s
 end
 
 def adding_matz
